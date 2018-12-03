e965f64b5234a7e68270632fa7c8017fc7af09d3
Misha <33575473+MishaKalinuyk@users.noreply.github.com>
2018-12-04 01:03:48 +0200

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D _rigidBody;
+
+    private void Start()
+    {
+        this._rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        this._rigidBody.AddForce(new Vector2(moveHorizontal, moveVertical) * this.speed);
+    }
+}
